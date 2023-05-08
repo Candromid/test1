d165b0be0b57623d6fbab1c7b095ff51b3eb21e9
HOME-PC\MTP <mainaccnurbek@mail.ru>
2023-05-08 14:34:08 +0500

    Init

---
+++ HomeWork6(class CoolRandom)/CoolRandom.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6_class_CoolRandom_
+{
+    public class CoolRandom
+    {
+        private Random random;
+        private int min;
+        private int max;
+        private List<int> values = new();
+
+        public CoolRandom(int min, int max)
+        {
+
+            this.random = new Random();
+            this.min = min;
+            this.max = max;
+       
+        }
+
+        public int Next()
+        {
+            if (values.Count == 0)
+            {
+                values = Enumerable.Range(min, max - min +1 ).ToList();
+            }
+
+            int index = random.Next(0, values.Count);
+            int value = values[index];
+            values.RemoveAt(index);
+
+            return value;
+        }
+
+               
+    }
+}
---
+++ HomeWork6(class CoolRandom)/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HomeWork6_class_CoolRandom_
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CoolRandom coolRandom = new CoolRandom(-5, 5);
+
+            for (int i = 0; i < 30; i++)
+            {
+                if (i % 10 == 0)
+                    Console.WriteLine(" - ");
+                int randomNumber = coolRandom.Next();
+                Console.WriteLine(randomNumber);
+            }
+
+            
+
+        }
+    }
+}
